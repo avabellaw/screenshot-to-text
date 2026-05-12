6e040719b39e65328eb11526c4d3c41eff2a7535
avabellaw <70448139+avabellaw@users.noreply.github.com>
2026-05-12 14:24:44 +0100

    Add install finished message

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ dnf install $DEPENDENCIES -y
 
 curl -sSL https://raw.githubusercontent.com/avabellaw/screenshot-to-text/refs/heads/main/screenshot.sh -o $INSTALL_DIR/screenshot
 chmod +x $INSTALL_DIR/screenshot
+
+if [[ $? -eq 0 ]]; then 
+    echo -e "\nInstalled successfully. \e[33mRun using command \"screenshot\" or bind command to keyboard shortcut.\033[0m";
+else 
+    echo -e "\nErrors during installation";
+fi
